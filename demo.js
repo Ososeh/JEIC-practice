6d1c1bd469dabda155830e4a1d8c959d599730d8
Ebewele Ososeh <61967799+Ososeh@users.noreply.github.com>
2026-07-09 16:01:18 +0100

    Add files via upload

--- demo.js
+++ demo.js
@@ -27,3 +27,39 @@ console.log("The new updated fruit list is:", fruits); // Banana,Orange,Apple
 The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:*/
 fruits.unshift("Lemon"); // Adds "Lemon" to the beginning of the array
 console.log("The new updated fruit list is:", fruits); // Lemon,Banana,Orange,Apple
+
+//Array.isArray()
+/* The Array.isArray() method returns true if an object is an array, otherwise false. */
+let text = "Hello World";
+let fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log("Is it an array?", Array.isArray(text)); // false
+console.log("Is it an array?", Array.isArray(fruits1)); // true
+
+// JavaScript Array concat()
+/* The concat() method creates a new array by merging (concatenating) existing arrays 
+The concat() method does not change the existing arrays. It always returns a new array.
+The concat() method can take any number of array arguments.*/
+let fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+let fruits3 = ["Lemon", "Grape"];
+let allFruits = fruits2.concat(fruits3);
+console.log("All fruits:", allFruits); // Banana,Orange,Apple,Mango,Lemon,Grape
+
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log("My children:", myChildren); // Cecilie,Lone,Emil,Tobias,Linus
+
+//JavaScript Array copyWithin()
+/* The copyWithin() method copies array elements to another position in the array, overwriting the existing values.
+The copyWithin() method overwrites the original array. 
+The copyWithin() method overwrites the existing values.
+The copyWithin() method does not add items to the array.
+The copyWithin() method does not change the length of the array.*/      
+
+const fruits4 = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Papaya", "Pineapple"];
+console.log("Before copyWithin:", fruits4);
+
+const fruits5 = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Papaya", "Pineapple"];
+fruits5.copyWithin(3, 0, 3);
+console.log("After copyWithin:", fruits5); // Banana,Orange,Banana,Orange,Kiwi
